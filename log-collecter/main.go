14fae20c2d5df78bc4fe13d735d3022ac6147f57
Arda Dağdeviren <rdty222@gmail.com>
2026-06-16 14:27:19 +0300

    feat: log collector servisi tamamlandi ve dockerize edildi

--- log-collecter/main.go
+++ log-collecter/main.go
@@ -1,51 +1,69 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"encoding/json"
+	"fmt"
 	"net/http"
-    // İleride buraya "os", "net/http" ve "encoding/json" da eklenecek
+	"os"
+	"time"
 )
 
-type logVerisi{
-	Mesaj  string 'json:"message"'
-	Seviye string 'json:"level"'
-	Zaman  time.Time 'json:"timestamp"'
-	Servis string 'json:"service"'
+var logFile *os.File
+
+type logVerisi struct {
+	Mesaj  string    `json:"message"` //logu tanımladım
+	Seviye string    `json:"level"`
+	Zaman  time.Time `json:"timestamp"`
+	Servis string    `json:"service"`
 }
 
+func main() {
+	var err error
 
-func main(){
+	logFile, err = os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 
-	http.HandleFunc("/logs", LogHandler)
-
-
-	func logHandler(w http.ResponseWriter ,r *http.Request){
-
-		if r.Method != http.MethodPost{
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			w.Write([]byte("Sadece post istekleri kabul edilir"))
-
-			return
-		}
-		defer r.Body.Close()
-
-		var yeniLogVerisi logVerisi
-		decoder :=json.NewDecoder(r.body)
-		err :=decoder.Decode(&logVerisi)
-
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("Gönderdiğiniz JSON formatı hatalı"))
-			return
-		}
-
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte("Log başarılı bir şekilde alındı"))
-
-
-
+	if err != nil {
+		fmt.Println("Dosya açılırken hata çıktı:", err) //dosyayı oluşturdum varsa üzerine yazdım, izinleri verdim
+		return
 	}
 
+	http.HandleFunc("/logs", logHandler) // logs portuna bakanı logHandlera yolladım
+
+	fmt.Println("Log toplayici 8080 portunda baslatiliyor...")
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func logHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed) //Sadece POST ları aldım güvenlik amaçlı
+		w.Write([]byte("Sadece post istekleri kabul edilir"))
+
+		return
+	}
+	defer r.Body.Close() //açılan Bodyi kapattım
+
+	var yeniLogVerisi logVerisi
+	decoder := json.NewDecoder(r.Body) //bodyi okuyacak yeni decoder oluşturdum
+	err := decoder.Decode(&yeniLogVerisi)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Gönderdiğiniz JSON formatı hatalı"))
+		return
+	}
+	logSatiri := fmt.Sprintf("[%v] [%s] [%s] %s\n", yeniLogVerisi.Zaman, yeniLogVerisi.Servis, yeniLogVerisi.Seviye, yeniLogVerisi.Mesaj)
+	//log satını string yazdım ilerde rahat okumak için
+
+	_, err = logFile.WriteString(logSatiri) //logFile dosyasına yazdım
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Log dosyaya yazilirken bir hata oluştu."))
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Log başarılı bir şekilde alındı"))
 
 }
